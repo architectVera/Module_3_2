70b3b4f76acb0f1b119dc0c5fc30f03212603677
architectVera <103371149+architectVera@users.noreply.github.com>
2022-12-07 12:30:03 +0100

    Add __repr__, __str__, __float__ methods. Validation check

--- Module3_2.py
+++ Module3_2.py
@@ -1,3 +1,5 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 
 class Product:
@@ -11,9 +13,18 @@ class Product:
         return float(self.price*quantity)
 
     def __eq__(self, other):
-        if self.name == other.name and self.price == other.price:
-            return True
-        return False
+        if not isinstance(other, Product):
+            return False
+        return self.name == other.name and self.price == other.price
+
+    def __repr__(self):
+        return f'Product (name = {self.name}, price = {self.price})'
+
+    def __str__(self):
+        return f'Name of product  is {self.name}, its price is {self.price})'
+
+    def __float__(self):
+        return  float(self.price)
 
 
 class ShoppingCart():
@@ -36,8 +47,18 @@ class ShoppingCart():
         self.cart = []
         self.quantity_cart = []
 
+    def __repr__(self):
+        return f'ShoppingCart (cart = {self.cart}, quantity_cart = {self.quantity_cart})'
+
+    def __float__(self):
+        return  self.calc_total()
+
     def add_to_cart(self, product: Product, quantity: int):
         """Adds  a product to the cart and adds the quantity to the quantity_cart."""
+
+        if not isinstance(product, Product):
+            raise RuntimeError("You can only add instances of the class Product")
+
         if product in self.cart:
             index_product = self.cart.index(product)
             self.quantity_cart[index_product] += quantity
@@ -60,7 +81,8 @@ class ShoppingCart():
         joined_cart = ShoppingCart()
         joined_cart.cart += self.cart
         joined_cart.quantity_cart += self.quantity_cart
-
+        if not isinstance(other, ShoppingCart):
+            raise RuntimeError("You can only add to instances of the class ShoppingCart")
         for product, quantity in zip(other.cart, other.quantity_cart):
             joined_cart.add_to_cart(product, quantity)
             return joined_cart
@@ -83,3 +105,6 @@ print(our_cart.quantity_cart)
 for p in our_cart.cart:
     print(p.name)
 print(our_cart.calc_total())
+print(apple.__repr__())
+print(apple.__str__())
+print(our_cart.__float__())
